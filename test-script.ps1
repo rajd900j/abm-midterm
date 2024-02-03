3ab0dd7b2b92ac5a3d99552cec2248cd06c5282b
Suman Sam <suman.sambrani14@gmail.com>
2024-02-02 19:08:36 -0700

    Initial commit

---
+++ test-script.ps1
@@ -0,0 +1,22 @@
+﻿function CreateABMFiles {
+
+$targetDirectory = "D:\powersheelscript"
+
+$numberOfFiles = 10
+
+1..$numberOfFiles | ForEach-Object {
+
+$fileName = "abm-$_.txt"
+
+
+$filePath = Join-Path -Path $targetDirectory -ChildPath $fileName 
+
+New-Item -Path $filePath -ItemType File -Force
+
+}
+
+
+}
+
+
+CreateABMFiles
